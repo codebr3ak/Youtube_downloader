e816b8ba33bf20a759bb1d73d9f3dc487dcee8c8
codebr3ak <barakaralph@gmail.com>
2021-08-01 11:19:29 +0300

    create the main window

--- yt_downloader.py
+++ yt_downloader.py
@@ -32,4 +32,17 @@ root.config(bg='Coral')
 #Heading label
 Label(root, text='TechVidvan Youtube Video Downloader', font=("Comic Sans MS", 15), bg='Coral').place(relx=0.25, rely=0.0)
 
+# Creating the main window
+Label(root, text='Enter the Youtube link:', font=("Times New Roman", 13), bg='Coral').place(relx=0.05, rely=0.2)
+link_strvar = StringVar(root)
+link_entry = Entry(root, width=50, textvariable=link_strvar)
+link_entry.place(relx=0.5, rely=0.2)
+Label(root, text='Enter the save location:', font=("Times New Roman", 13), bg='Coral').place(relx=0.05, rely=0.4)
+dir_strvar = StringVar(root)
+dir_entry = Entry(root, width=50, textvariable=dir_strvar)
+dir_entry.place(relx=0.5, rely=0.4)
+Label(root, text='Enter the filename:', font=("Times New Roman", 13), bg='Coral').place(relx=0.05, rely=0.6)
+filename_strvar = StringVar(root)
+filename_entry = Entry(root, width=50, textvariable=filename_strvar)
+filename_entry.place(relx=0.5, rely=0.6)
 
